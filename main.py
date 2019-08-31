7c1e3d29737392b5ad61d374528a7706b7eba6eb
eademir <eraydemir1@yandex.com>
2019-08-31 16:20:40 +0300

    added scipy, pylab, statsmodels.api libs

--- main.py
+++ main.py
@@ -40,6 +40,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.stats import norm, shapiro
+import seaborn as sns
+from scipy import stats
+import pylab as pl
+import statsmodels.api as sm
+
 ax = plt.gca()
 
 iris=pd.read_csv("iris.csv")
@@ -167,14 +173,39 @@ print(df.max())
 print(df.cumsum())
 #print(df.cumprod())
 
+# =============================================================================
+# popülasyon normal dağılımdan geliyorsa, eminsek, z-testi
+# popülasyon normal dağılımdan gelmiyorsa t-test
+# =============================================================================
 
+veri_normal = norm.rvs(size = 10000, loc = 0, scale = 1)
 
+ax = sns.distplot(veri_normal)
+ax = sns.distplot(veri_normal, bins = 100, kde = False, color = "orange", hist_kws = {"linewidth": 15, "alpha":1})
 
+ax.set(xlabel = "Normal", ylabel = "Frekans")
 
+#t-testi
 
+olcumler = np.array([17, 160, 234, 149, 145, 107, 197, 75, 201, 225, 211, 119, 
+               157, 145, 127, 244, 163, 114, 145,  65, 112, 185, 202, 146,
+               203, 224, 203, 114, 188, 156, 187, 154, 177, 95, 165, 50, 110, 
+       216, 138, 151, 166, 135, 155, 84, 251, 173, 131, 207, 121, 120])
+sns.distplot(olcumler)
+stats.describe(olcumler)
+pd.DataFrame(olcumler).plot.hist()
 
+stats.probplot(olcumler, dist = "norm", plot = pl, )
+pl.show()
 
+sm.qqplot(olcumler, line = "s")
+pl.show()
 
+#tests shopiro-wilks
+
+stat, p = shapiro(olcumler)
+
+print("Statistics = %.3f, p = %.3f" % (stat, p))
 
 
 
